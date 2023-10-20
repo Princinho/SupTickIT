a87979044f93e5383ee3bafcdcf96800c9012d67
Gatien GNAKOU-EDJAMBO <princegnakou@gmail.com>
2023-10-20 17:16:44 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+
+
+function App() {
+  return (
+    <>
+      Hellow World!
+    </>
+  )
+}
+
+export default App
---
+++ src/main.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
+import { MainLayout } from './MainLayout.jsx'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<MainLayout />} >
+
+    </Route>
+  )
+)
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
